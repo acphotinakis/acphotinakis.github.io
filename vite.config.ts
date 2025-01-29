e2061024fb6793fe8cc79e5041e2b0cb13458375
Andrew Photinakis <acp7795@rit.edu>
2025-01-29 11:35:51 +0100

    continuing installation of magicUI

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import CONFIG from './gitprofile.config';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -42,6 +43,12 @@ export default defineConfig({
         ]
       : []),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
   define: {
     CONFIG: CONFIG,
   },
